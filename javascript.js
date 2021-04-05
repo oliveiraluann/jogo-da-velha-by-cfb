f03da5fe8c3400fc67d7bdfad0f51413db0f8029
Luan Oliveira <luan.so@live.com>
2021-04-05 05:42:13 -0300

    Funções para iniciar o jogo e quem joga iniciada

--- javascript.js
+++ javascript.js
@@ -6,3 +6,26 @@ let jogando=true;
 let nivel=1;
 let jogadaCpu=1;
 let quemComeca;
+
+function jogar(p) {
+    if(jogando && quemJoga==0) {
+        switch(p) {
+            case 1:
+                if(jogo[0][0]=="") {
+                    jogo[0][0]="X";
+                    quemJoga=1;
+                }
+            break;
+        }
+    }
+}
+
+function iniciar() {
+    jogando=true;
+    jogadaCpu=1;
+    jogo=[
+        ["","",""],
+        ["","",""],
+        ["","",""]
+    ];
+}

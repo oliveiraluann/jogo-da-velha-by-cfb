9d2d872bd562aa8c2c24e211bf141df0536c725e
Luan Oliveira <luan.so@live.com>
2021-04-06 05:30:21 -0300

    Posições no Jogo da Velha definidas

--- javascript.js
+++ javascript.js
@@ -15,7 +15,55 @@ function jogar(p) {
                     jogo[0][0]="X";
                     quemJoga=1;
                 }
-            break;
+                break;
+            case 2:
+                if(jogo[0][1]=="") {
+                    jogo[0][1]="X";
+                    quemJoga=1;
+                }
+                break;
+            case 3:
+                if(jogo[0][2]=="") {
+                    jogo[0][2]="X";
+                    quemJoga=1;
+                }
+                break;
+            case 4:
+                if(jogo[1][0]=="") {
+                    jogo[1][0]="X";
+                    quemJoga=1;
+                }
+                break;
+            case 5:
+                if(jogo[1][1]=="") {
+                    jogo[1][1]="X";
+                    quemJoga=1;
+                }
+                break;
+            case 6:
+                if(jogo[1][2]=="") {
+                    jogo[1][2]="X";
+                    quemJoga=1;
+                }
+                break;
+            case 7:
+                if(jogo[2][0]=="") {
+                    jogo[2][0]="X";
+                    quemJoga=1;
+                }
+                break;
+            case 8:
+                if(jogo[2][1]=="") {
+                    jogo[2][1]="X";
+                    quemJoga=1;
+                }
+                break;
+            default:
+                if(jogo[2][2]=="") {
+                    jogo[2][2]="X";
+                    quemJoga=1;
+                }
+                break;
         }
     }
 }
@@ -29,3 +77,5 @@ function iniciar() {
         ["","",""]
     ];
 }
+
+window.addEventListener("load", iniciar);
